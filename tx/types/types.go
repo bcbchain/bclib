87b852f53e2c1253fa6d05e91cc74e781315607a
lixin <lixino727@qq.com>
2020-08-11 19:48:00 +0800

    add tx type

---
+++ tx/types/types.go
@@ -0,0 +1,23 @@
+package types
+
+import (
+	"github.com/bcbchain/bclib/tendermint/go-crypto"
+	tx1 "github.com/bcbchain/bclib/tx/v1"
+	type2 "github.com/bcbchain/bclib/types"
+)
+
+type TxV1Result struct {
+	FromAddr    crypto.Address
+	Pubkey      crypto.PubKeyEd25519
+	Transaction tx1.Transaction
+}
+
+type TxV2Result struct {
+	Pubkey      crypto.PubKeyEd25519
+	Transaction type2.Transaction
+}
+
+type TxV3Result struct {
+	Pubkey      crypto.PubKeyEd25519
+	Transaction type2.Transaction
+}
